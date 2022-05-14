c927fe1027a56182ca700844d89955986b2149a3
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-05-14 14:18:20 -0400

    Initial Commit

---
+++ HackHillsEm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackHillsEm
+//
+//  Created by Ekam Ghai on 5/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HackHillsEm/HackHillsEmApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackHillsEmApp.swift
+//  HackHillsEm
+//
+//  Created by Ekam Ghai on 5/14/22.
+//
+
+import SwiftUI
+
+@main
+struct HackHillsEmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
